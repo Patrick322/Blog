8299ffc6a82cdf3056d472aa766200c6a68320a3
Patrick595 <patrickbuong@gmail.com>
2020-02-20 12:15:40 +0300

    created class post which will encounter new blog post

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin)
+class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(20),unique=True,nullable=False)
@@ -20,7 +20,7 @@ class User(db.Model,UserMixin)
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
 
-class Post(db.Model):
+class Post(db.Model)
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key= True)
     title = db.Column(db.String(100),nullable=False)
