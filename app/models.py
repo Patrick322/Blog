a786a4e7b251c6a1edbc64ff9f5bc67c4e1e8aa6
Patrick595 <patrickbuong@gmail.com>
2020-02-20 12:14:10 +0300

    created class user

--- app/models.py
+++ app/models.py
@@ -7,12 +7,12 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin):
+class User(db.Model,UserMixin)
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(20),unique=True,nullable=False)
     email = db.Column(db.String(120),unique=True,nullable=False)
-    # image_file = db.Column(db.String(20),nullable =False, default='default.jpg')
+    image_file = db.Column(db.String(20),nullable =False, default='default.jpg')
     password = db.Column(db.String(60),nullable=False)
     posts = db.relationship('Post',backref= 'author',lazy=True)
     comments = db.relationship('Comment', backref='author', lazy= True)
@@ -27,7 +27,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
     summary = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    # image_file = db.Column(db.String(20), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False)
     category = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'), nullable=False)
     upvote = db.Column(db.Integer, nullable=False, default = 0)
