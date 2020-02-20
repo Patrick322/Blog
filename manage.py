9c33534c66c65c3412f7d3bbc17cb520157745cc
Patrick595 <patrickbuong@gmail.com>
2020-02-20 12:03:19 +0300

    corrected errors of spacing and indentation

---
+++ manage.py
@@ -0,0 +1,12 @@
+from app import create_app
+from flask_script import Manager, Server
+from flask_migrate import MigrateCommand
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
